ba91f04e0986c77d68024ac04472693c3833df34
旭彤 <xutong.chen@wormpex.com>
2023-07-01 15:45:02 +0800

    sd 抽象

--- web/python_template_server.py
+++ web/python_template_server.py
@@ -7,7 +7,7 @@ import json
 import sys
 
 from flask import Flask, Response, request
-
+from datetime import datetime
 sys.path.append('.')
 from utils.log import get_logger
 from utils.decorator import web_exception_handler
@@ -33,6 +33,16 @@ charset = 'utf8'
 
 dbm = DBManager(host=host,port=port,user=user,password=password,database=database,charset=charset)
 
+sql_dict = {
+    "update_status":"update sd_task set status={} where id={}",
+    "submit_result":"update sd_task set status=2, content='{}' where id={}",
+    "user_used_count":"select count(1) from sd_task where user_name='{}' and TO_DAYS(create_time) = TO_DAYS(NOW())",
+    "has_user":"select * from wp_users where user_login='{}'",
+    "submit_task":"insert into sd_task(user_name,param, status) values (%s,%s,%s)",
+    "task_list":"select * from sd_task where user_name='{}' order by id desc limit 20",
+    "last_task":"select * from sd_task where user_name='{}' order by id desc limit 1"
+}
+
 
 
 @app.route('/vision/python/template/hello', methods=['GET', 'POST'])
@@ -44,15 +54,6 @@ def hello():
 
     print('hello, hello, 测试接口')
     return Response(json.dumps({'msg': 'hello', 'status': 0, 'data': {}}), mimetype='application/json', status=200)
-
-
-@app.route('/vision/python/template/example/v1', methods=['POST', 'GET'])
-@web_exception_handler
-def example_v1():
-    param = request.get_json()
-    # do anything here
-    return Response(json.dumps({'msg': 'success', 'status': 200, 'data': {}}),
-                    mimetype='application/json', status=200)
 
 
 
@@ -77,8 +78,7 @@ def get_undo_task():
     logger.info("undo_task send: {}".format(res_list_p))
     if len(res_list) != 0:
         id = res_list[0][0]
-        sql2 = "update sd_task set status=1 where id={}".format(id)
-        dbm.update(sql2)
+        dbm.update(sql_dict["update_status"].format(1, id))
     return Response(json.dumps({'msg': 'success', 'status': 0, 'data': res_list_p}, ensure_ascii=False), mimetype='application/json',
                     status=200)
 
@@ -95,8 +95,7 @@ def collect_result():
     image_url = f"http://aistormy.com/vision/{file.filename}"
 
     logger.info("collect_result: {}, image_url:{}".format(task_id, image_url))
-    sql = "update sd_task set status=2, content='{}' where id={}".format(image_url, task_id)
-    dbm.update(sql)
+    dbm.update(sql_dict["submit_result"].format(task_id, image_url))
     return Response(json.dumps({'msg': 'success', 'status': 0, 'data': {}}, ensure_ascii=False), mimetype='application/json',
                     status=200)
 
@@ -106,14 +105,11 @@ def collect_result():
 def check_username(user_name):
     if user_name == "":
         return False, '用户不存在，请先注册'
-    sql_check1 = "select * from wp_users where user_login='{}'".format(user_name)
-    logger.info(sql_check1)
-    res1 = dbm.query(sql_check1)
+    res1 = dbm.query(sql_dict["has_user"].format(user_name))
     logger.info("user_true_check:{}".format(res1))
     if len(res1) == 0:
         return False, '用户不存在，请先注册'
-    sql_check2 = "select count(1) from sd_task where user_name='{}' and TO_DAYS(create_time) = TO_DAYS(NOW())".format(user_name)
-    res2 = dbm.query(sql_check2)
+    res2 = dbm.query(sql_dict["user_used_count"].format(user_name))
     logger.info("user_num_check:{}".format(res2))
     if res2[0][0] >= 20:
         return False, '您所使用的用户，今日使用次数已达上限20次, 如需大量使用请联系管理员assassin.creed.wax@gmail.com'
@@ -130,7 +126,7 @@ def submit_task():
     logger.info("submit_task: {}".format(param_dict))
     user_name = param_dict['user_name']
     isOk, msg = check_username(user_name)
-    logger.info("提交结果:{}, {}".format(isOk, msg))
+    logger.info("用户验证结果:{}, {}".format(isOk, msg))
     if not isOk:
         return Response(json.dumps({'msg': msg, 'status': 1, 'data': {}}, ensure_ascii=False),
                         mimetype='application/json',
@@ -138,11 +134,31 @@ def submit_task():
 
     data = [[user_name, json.dumps(param_dict, ensure_ascii=False), 0]]
 
-    sql = "insert into sd_task(user_name,param, status) values (%s,%s,%s)"
     logger.info("submit_task_sql: {}".format(data))
-    dbm.insert(sql, data)
+    dbm.insert(sql_dict["submit_task"], data)
     return Response(json.dumps({'msg': 'success', 'status': 0, 'data': {}}, ensure_ascii=False), mimetype='application/json',
                     status=200)
+
+@app.route('/vision/getTaskResult', methods=['POST', 'GET'])
+@web_exception_handler
+def get_task_result():
+    param_dict = request.get_json()
+    # logger.info("get_task_result: {}".format(param_dict))
+    user_name = param_dict['user_name']
+    res_list = dbm.query(sql_dict["last_task"].format(user_name))
+    if len(res_list) == 0:
+        return Response(json.dumps({'msg': '您没有进行中的任务', 'status': 2, 'data': {}}, ensure_ascii=False), mimetype='application/json',
+                    status=200)
+    if res_list[0][4] == 0 or res_list[0][4] == 1:
+        return Response(json.dumps({'msg': '您的任务在执行中，请耐心等待', 'status': 1, 'data': {}}, ensure_ascii=False), mimetype='application/json',
+                    status=200)
+    if res_list[0][4] == -1 or datetime.now() - res_list[0][6] > 600:
+        return Response(json.dumps({'msg': '您的任务执行失败或已超时', 'status': 2, 'data': {}}, ensure_ascii=False), mimetype='application/json',
+                    status=200)
+    if res_list[0][4] == 2:
+        return Response(json.dumps({'msg': '任务完成', 'status': 0, 'data': {'img': res_list[0][3]}}, ensure_ascii=False), mimetype='application/json',
+                    status=200)
+
 
 
 ##  列表页查询
@@ -152,8 +168,7 @@ def my_task_list():
     param_dict = request.get_json()
     logger.info("task_list_query: {}".format(param_dict))
     user_name = param_dict['user_name']
-    sql = "select * from sd_task where user_name='{}' order by id desc limit 20".format(user_name)
-    res_list = dbm.query(sql)
+    res_list = dbm.query(sql_dict["task_list"].format(user_name))
     res_list_p = []
     for res in res_list:
         res_p = [res[0], res[1], res[2], res[3], res[4], res[5].strftime("%Y-%m-%d %H:%M:%S"), res[6].strftime("%Y-%m-%d %H:%M:%S")]
