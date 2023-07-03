031f1e6735e12f1147a85750d99df0e3b855246e
李顺东 <lishundong@lishundongdeMacBook-Pro.local>
2023-07-03 19:40:18 +0800

    chat部分bug修改

--- service/chat_service.py
+++ service/chat_service.py
@@ -59,10 +59,12 @@ class ChatDataSourceStrategy:
 
     def user_add_msg(self, source, user_name, msg):
         if user_name not in self.chat_conversations:
+            logger.info("user_add_msg user_name: {} not in chat_conversations".format(user_name))
             self.chat_conversations[user_name] = {}
 
         if source not in self.chat_conversations[user_name]:
-            self.chat_conversations[user_name][source] = {"time": time.time()}
+            logger.info("user_add_msg user_name: {} source: {} not in chat_conversations".format(user_name, source))
+            self.chat_conversations[user_name][source] = {"time": time.time(), "conversation": []}
 
         self.chat_conversations[user_name][source]["conversation"].append({"type": "user", "msg": msg})
         self.refresh_ttl(user_name, source)
@@ -97,13 +99,38 @@ class ChatTaskStrategy:
     def __init__(self):
         self.tasks = {}
         self.user_tasks = {}
-        self.robot_msg_tasks = {}
+        self.user_task_ttls = {}
         self.sources_task = {}
-        self.user_task_ttl = {}
+
+    def refresh_ttl(self, user_name, source):
+        self.user_task_ttls[user_name + "_" + source] = time.time()
+
+    def run(self):
+        while True:
+            try:
+                now_time = time.time()
+                deletes = []
+                for key, before_time in self.user_task_ttls:
+                    if now_time - before_time > 600:
+                        deletes.append(key)
+
+                for d in deletes:
+                    del self.user_task_ttls[d]
+                    task_ids = self.user_tasks[d]
+                    for task_id in task_ids:
+                        if task_id in self.tasks:
+                            del self.tasks[task_id]
+                        if task_id in self.sources_task:
+                            del self.sources_task[task_id]
+
+                    del self.user_tasks[d]
+                time.sleep(10)
+            except BaseException as e:
+                logger.error(e)
 
     def add_task(self, source, user_name, msg_index):
         task_id = uuid.uuid1()
-        self.tasks[task_id] = {"user_name": user_name, "source": source, "index": msg_index}
+        self.tasks[task_id] = {"user_name": user_name, "source": source, "user_index": msg_index, "robot_msg_index": 0}
         self.user_tasks[user_name + "_" + source] = task_id
 
         if source not in self.sources_task:
@@ -116,22 +143,19 @@ class ChatTaskStrategy:
         task_ids = self.user_tasks[user_name + "_" + source]
         del self.user_tasks[user_name + "_" + source]
 
-        if user_name + "_" + source in self.robot_msg_tasks:
-            del self.robot_msg_tasks[user_name + "_" + source]
-
         for task_id in task_ids:
             if task_id not in self.tasks:
                 return
             del self.tasks[task_id]
 
     def fetch_robot_msg_index(self, source, user_name):
-        if user_name + "_" + source not in self.robot_msg_tasks:
+        if user_name + "_" + source not in self.user_tasks:
             return 0
 
-        return self.robot_msg_tasks[user_name + "_" + source]
+        return self.user_tasks[user_name + "_" + source]["robot_msg_index"]
 
     def set_robot_msg_index(self, source, user_name, index):
-        self.robot_msg_tasks[user_name + "_" + source] = index
+        self.user_tasks[user_name + "_" + source]["robot_msg_index"] = index
 
     def get_new_task(self, source):
         if len(self.sources_task[source]) == 0:
@@ -140,7 +164,7 @@ class ChatTaskStrategy:
         task_id = self.sources_task[source][0]
         task_info = self.tasks[task_id]
 
-        return True, task_info["user_name"], task_info["source"], task_info["index"], task_id
+        return True, task_info["user_name"], task_info["source"], task_info["user_index"], task_id
 
     def finish_task(self, task_id):
         if task_id not in self.tasks:
